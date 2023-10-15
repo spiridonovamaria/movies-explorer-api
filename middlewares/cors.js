7ec5f249cce11b910acf7eb6ce4b5d6822564251
Maria Spiridonova <traffic2007@mail.ru>
2023-10-15 16:59:06 +0700

    pravki

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,4 @@
 const allowedCors = [
-  'https://api.moviesdiplom.nomoredomainsicu.ru',
-  'http://api.moviesdiplom.nomoredomainsicu.ru',
   'https://moviesdiplom.nomoredomainsicu.ru',
   'http://moviesdiplom.nomoredomainsicu.ru',
   'https://localhost:3000',
